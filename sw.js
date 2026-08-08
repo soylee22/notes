c9d355432009f3b22332abe84a19079ac4c9cdea
soylee22 <73914454+soylee22@users.noreply.github.com>
2026-08-08 10:58:03 +0100

    notes: 2026-08-08

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Reader service worker. Shell cache-first, payload network-first.
-const CACHE = 'reader-20260808105631';
+const CACHE = 'reader-20260808105801';
 const SHELL = ['./', './index.html', './manifest.webmanifest',
                './icon-180.png', './icon-192.png', './icon-512.png'];
 
